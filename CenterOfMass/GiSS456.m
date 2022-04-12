bf41dfa6a1b61ce3745a91e5cb59d5c80b95d2ee
Sylvain Marsat <marsat@apc.in2p3.fr>
2022-04-12 15:32:31 +0200

    Added CenterOfMass/GiSS456.m

---
+++ CenterOfMass/GiSS456.m
@@ -0,0 +1,66 @@
+(-3*G^2*m1^2*m2*n12i[i])/(2*c^4*r12[]) + (3*G^2*m1*m2^2*n12i[i])/
+  (2*c^4*r12[]) + (3*G*m2*\[Kappa]1*n12i[a]*S1ij[-a, -b]*S1ij[i, b])/
+  (2*c^6*m1*r12[]^2) - (3*G*n12i[a]*S1ij[i, b]*S2ij[-a, -b])/
+  (2*c^6*r12[]^2) + (3*G*n12i[a]*S1ij[-a, b]*S2ij[i, -b])/(2*c^6*r12[]^2) - 
+ (3*G*m1*\[Kappa]2*n12i[a]*S2ij[-a, -b]*S2ij[i, b])/(2*c^6*m2*r12[]^2) - 
+ (G*m2*\[Kappa]1*n12i[i]*Scalar[S1ij[-a, -b]*S1ij[a, b]])/
+  (2*c^6*m1*r12[]^2) + (G*m1*\[Kappa]2*n12i[i]*
+   Scalar[S2ij[-a, -b]*S2ij[a, b]])/(2*c^6*m2*r12[]^2) - 
+ (G*m2*n12i[a]*S1ij[i, -a]*Scalar[n12i[a]*v1i[-a]])/(c^5*r12[]) - 
+ (G*m1*n12i[a]*S2ij[i, -a]*Scalar[n12i[a]*v1i[-a]])/(c^5*r12[]) - 
+ (G*m1*m2*n12i[i]*Scalar[n12i[a]*v1i[-a]]^2)/(8*c^4) + 
+ (5*G*m1*m2*n12i[i]*Scalar[v1i[-a]*v1i[a]])/(8*c^4) - 
+ (3*G*m2*n12i[i]*Scalar[n12i[a]*S1ij[-a, -b]*v1i[b]])/(4*c^5*r12[]) + 
+ (G*m1*n12i[i]*Scalar[n12i[a]*S2ij[-a, -b]*v1i[b]])/(c^5*r12[]) - 
+ (G*m2*n12i[a]*S1ij[i, -a]*Scalar[n12i[a]*v2i[-a]])/(c^5*r12[]) - 
+ (G*m1*n12i[a]*S2ij[i, -a]*Scalar[n12i[a]*v2i[-a]])/(c^5*r12[]) + 
+ (G*m1*m2*n12i[i]*Scalar[n12i[a]*v2i[-a]]^2)/(8*c^4) - 
+ (5*G*m1*m2*n12i[i]*Scalar[v2i[-a]*v2i[a]])/(8*c^4) + 
+ (G*m2*n12i[i]*Scalar[n12i[a]*S1ij[-a, -b]*v2i[b]])/(c^5*r12[]) - 
+ (3*G*m1*n12i[i]*Scalar[n12i[a]*S2ij[-a, -b]*v2i[b]])/(4*c^5*r12[]) + 
+ (S1ij[i, -a]*v1i[a])/c^3 + (4*G*m2*S1ij[i, -a]*v1i[a])/(c^5*r12[]) - 
+ (2*G*m1*S2ij[i, -a]*v1i[a])/(c^5*r12[]) - 
+ (7*G*m1*m2*Scalar[n12i[a]*v1i[-a]]*v1i[i])/(4*c^4) - 
+ (7*G*m1*m2*Scalar[n12i[a]*v2i[-a]]*v1i[i])/(4*c^4) - 
+ (2*G*m2*S1ij[i, -a]*v2i[a])/(c^5*r12[]) + (S2ij[i, -a]*v2i[a])/c^3 + 
+ (4*G*m1*S2ij[i, -a]*v2i[a])/(c^5*r12[]) + 
+ (7*G*m1*m2*Scalar[n12i[a]*v1i[-a]]*v2i[i])/(4*c^4) + 
+ (7*G*m1*m2*Scalar[n12i[a]*v2i[-a]]*v2i[i])/(4*c^4) + m1*y1i[i] + 
+ (G^2*m1^2*m2*y1i[i])/(4*c^4*r12[]^2) + (G^2*m1*m2^2*y1i[i])/
+  (4*c^4*r12[]^2) - (G*m1*m2*y1i[i])/(2*c^2*r12[]) + 
+ (G*m2*\[Kappa]1*Scalar[S1ij[-a, -b]*S1ij[a, b]]*y1i[i])/(4*c^6*m1*r12[]^3) - 
+ (3*G*m2*\[Kappa]1*Scalar[n12i[a]*n12i[b]*S1ij[-a, i]*S1ij[-b, -i]]*y1i[i])/
+  (4*c^6*m1*r12[]^3) + (G*Scalar[S1ij[a, b]*S2ij[-a, -b]]*y1i[i])/
+  (2*c^6*r12[]^3) + (G*m1*\[Kappa]2*Scalar[S2ij[-a, -b]*S2ij[a, b]]*y1i[i])/
+  (4*c^6*m2*r12[]^3) - (3*G*Scalar[n12i[a]*n12i[b]*S1ij[-a, i]*S2ij[-b, -i]]*
+   y1i[i])/(2*c^6*r12[]^3) - 
+ (3*G*m1*\[Kappa]2*Scalar[n12i[a]*n12i[b]*S2ij[-a, i]*S2ij[-b, -i]]*y1i[i])/
+  (4*c^6*m2*r12[]^3) + (m1*Scalar[v1i[-a]*v1i[a]]*y1i[i])/(2*c^2) + 
+ (7*G*m1*m2*Scalar[v1i[-a]*v1i[a]]*y1i[i])/(4*c^4*r12[]) + 
+ (3*m1*Scalar[v1i[-a]*v1i[a]]^2*y1i[i])/(8*c^4) + 
+ (3*G*m2*Scalar[n12i[a]*S1ij[-a, -b]*v1i[b]]*y1i[i])/(4*c^5*r12[]^2) - 
+ (2*G*m1*Scalar[n12i[a]*S2ij[-a, -b]*v1i[b]]*y1i[i])/(c^5*r12[]^2) - 
+ (G*m1*m2*Scalar[n12i[a]*v1i[-a]]*Scalar[n12i[a]*v2i[-a]]*y1i[i])/
+  (4*c^4*r12[]) - (7*G*m1*m2*Scalar[v1i[a]*v2i[-a]]*y1i[i])/(4*c^4*r12[]) - 
+ (G*m1*m2*Scalar[v2i[-a]*v2i[a]]*y1i[i])/(4*c^4*r12[]) - 
+ (2*G*m2*Scalar[n12i[a]*S1ij[-a, -b]*v2i[b]]*y1i[i])/(c^5*r12[]^2) - 
+ (G*m1*Scalar[n12i[a]*S2ij[-a, -b]*v2i[b]]*y1i[i])/(4*c^5*r12[]^2) + 
+ m2*y2i[i] + (G^2*m1^2*m2*y2i[i])/(4*c^4*r12[]^2) + 
+ (G^2*m1*m2^2*y2i[i])/(4*c^4*r12[]^2) - (G*m1*m2*y2i[i])/(2*c^2*r12[]) + 
+ (G*m2*\[Kappa]1*Scalar[S1ij[-a, -b]*S1ij[a, b]]*y2i[i])/(4*c^6*m1*r12[]^3) - 
+ (3*G*m2*\[Kappa]1*Scalar[n12i[a]*n12i[b]*S1ij[-a, i]*S1ij[-b, -i]]*y2i[i])/
+  (4*c^6*m1*r12[]^3) + (G*Scalar[S1ij[a, b]*S2ij[-a, -b]]*y2i[i])/
+  (2*c^6*r12[]^3) + (G*m1*\[Kappa]2*Scalar[S2ij[-a, -b]*S2ij[a, b]]*y2i[i])/
+  (4*c^6*m2*r12[]^3) - (3*G*Scalar[n12i[a]*n12i[b]*S1ij[-a, i]*S2ij[-b, -i]]*
+   y2i[i])/(2*c^6*r12[]^3) - 
+ (3*G*m1*\[Kappa]2*Scalar[n12i[a]*n12i[b]*S2ij[-a, i]*S2ij[-b, -i]]*y2i[i])/
+  (4*c^6*m2*r12[]^3) - (G*m1*m2*Scalar[v1i[-a]*v1i[a]]*y2i[i])/
+  (4*c^4*r12[]) + (G*m2*Scalar[n12i[a]*S1ij[-a, -b]*v1i[b]]*y2i[i])/
+  (4*c^5*r12[]^2) + (2*G*m1*Scalar[n12i[a]*S2ij[-a, -b]*v1i[b]]*y2i[i])/
+  (c^5*r12[]^2) - (G*m1*m2*Scalar[n12i[a]*v1i[-a]]*Scalar[n12i[a]*v2i[-a]]*
+   y2i[i])/(4*c^4*r12[]) - (7*G*m1*m2*Scalar[v1i[a]*v2i[-a]]*y2i[i])/
+  (4*c^4*r12[]) + (m2*Scalar[v2i[-a]*v2i[a]]*y2i[i])/(2*c^2) + 
+ (7*G*m1*m2*Scalar[v2i[-a]*v2i[a]]*y2i[i])/(4*c^4*r12[]) + 
+ (3*m2*Scalar[v2i[-a]*v2i[a]]^2*y2i[i])/(8*c^4) + 
+ (2*G*m2*Scalar[n12i[a]*S1ij[-a, -b]*v2i[b]]*y2i[i])/(c^5*r12[]^2) - 
+ (3*G*m1*Scalar[n12i[a]*S2ij[-a, -b]*v2i[b]]*y2i[i])/(4*c^5*r12[]^2)
